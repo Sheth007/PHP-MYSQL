5321dd3e3e7da017661ae743c48e5405cac2960d
Sheth Uday <shethuday505@gmail.com>
2026-01-21 14:52:14 +0530

    Alter table queries

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // include_once('./Queries/Create_table.php');
 // include_once('Queries/Insert_data.php');
-include_once('Queries/select_data.php');
+// include_once('Queries/select_data.php');
+include_once('Queries/Alter_table.php');
 ?>
