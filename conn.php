8fdca3a3dbd97d75c6cead20c12550839ec89654
Sheth Uday <shethuday505@gmail.com>
2026-01-21 12:29:37 +0530

    Insert the recordss

--- conn.php
+++ conn.php
@@ -8,8 +8,6 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Database Connected Successfully";
 }
 
 // $sql = "CREATE DATABASE training";
